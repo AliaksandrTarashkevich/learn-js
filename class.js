e3907f88ab4b6fa1bb5361371fc14471498d0b08
AliaksandrTarashkevich <alexandertarashkevich@gmail.com>
2020-03-24 20:02:42 +0300

    Added html and made a console clock! IMPORTANT

--- class.js
+++ class.js
@@ -61,12 +61,49 @@
 
 // user = new User('');
 //?======================================
-class User {
-  name = "Anonynous";
+// class User {
+//   name = "Anonynous";
 
-  sayHi() {
-    console.log(`Hi, ${this.name}!`);
+//   sayHi() {
+//     console.log(`Hi, ${this.name}!`);
+//   }
+// }
+
+// new User().sayHi();
+//?========================================
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let seconds = date.getSeconds();
+    if (seconds < 10) seconds = '0' + seconds;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', seconds);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
   }
 }
 
-new User().sayHi();
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
