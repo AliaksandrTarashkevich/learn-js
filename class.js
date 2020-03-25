b107ff1b73bf192a76d40269c8f13c81ac3ff28c
AliaksandrTarashkevich <alexandertarashkevich@gmail.com>
2020-03-25 11:34:39 +0300

    added a clock with func method

--- class.js
+++ class.js
@@ -2,31 +2,29 @@
 //   constructor(name) {
 //     this.name = name;
 //   }
-
+  
 //   sayHi() {
 //     console.log(this.name);
 //   }
 // }
 
-// let user = new User('Ivan');
+// let user = new User('John');
 // user.sayHi();
-//?===================================
-// class User {
-//   constructor(name) {
-//     this.name = name;
+//?=====================================
+// let User = class {
+//   sayHi() {
+//     console.log('Whaza');
 //   }
-//   sayHi(){ console.log(this.name); }
-// }
-
-// console.log(typeof User);
-//?====================================
-// let User = class MyClass {
+// };
+//?=====================================
+// let User = class MyClass{
 //   sayHi() {
 //     console.log(MyClass);
 //   }
 // };
 
 // new User().sayHi();
+// console.log(MyClass);//!ERROR, cannot see the property outside of class
 //?=====================================
 // function makeClass(phrase) {
 //   return class {
@@ -36,74 +34,37 @@
 //   };
 // }
 
-// let User = makeClass('Whaza');
+// let User = makeClass('KUKU');
 // new User().sayHi();
-//?=====================================
+//?======================================
 // class User {
-//   constructor(name) {
+//   constructor (name) {
 //     this.name = name;
 //   }
+
 //   get name() {
 //     return this._name;
 //   }
+
 //   set name(value) {
-//     if(value.length < 4) {
+//     if (value.length < 4) {
 //       console.log('Too small =(');
 //       return;
 //     }
 //     this._name = value;
 //   }
-
 // }
 
-// let user = new User('Ivan');
+// let user = new User('Kolya');
 // console.log(user.name);
-
 // user = new User('');
-//?======================================
+//?====================
 // class User {
-//   name = "Anonynous";
+//   name = 'Anonymous';
 
 //   sayHi() {
-//     console.log(`Hi, ${this.name}!`);
+//     console.log(`Hello, ${this.name}!`);
 //   }
 // }
 
 // new User().sayHi();
-//?========================================
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-  }
-  render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
-
-    let seconds = date.getSeconds();
-    if (seconds < 10) seconds = '0' + seconds;
-
-    let output = this.template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', seconds);
-
-    console.log(output);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), 1000);
-  }
-}
-
-let clock = new Clock({template: 'h:m:s'});
-clock.start();
