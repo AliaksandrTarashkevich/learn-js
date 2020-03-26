2b85021fe9883d4e02c2fc7547ad8e61e29026ce
AlexTar <alexandertarashkevich@gmail.com>
2020-03-26 22:32:14 +0300

    mixins added

--- private.js
+++ private.js
@@ -18,15 +18,47 @@
 // let coffeeMachine = new CoffeeMachine(100);
 // coffeeMachine.waterAmount = -10;
 //?=========================================================
-class CoffeeMachine {
-    constructor(power) {
-        this._power = power;
+// class CoffeeMachine {
+//     constructor(power) {
+//         this._power = power;
+//     }
+
+//     get power() {
+//         return this._power;
+//     }
+// }
+
+// let coffeeMachine = new CoffeeMachine(100);
+// console.log(`Power is ${coffeeMachine.power}W`);
+//=========================================================
+// class CoffeeMachine {
+//     _waterAmount = 0;
+
+//     setWaterAmount(value) {
+//         if(value < 0) throw new Error('Sub zero amount of water');
+//         this._waterAmount = value;
+//     }
+
+//     getWaterAmount() {
+//         return this._waterAmount;
+//     }
+// }
+
+// new CoffeeMachine().setWaterAmount(100);
+//==========================================================
+class CoffeeMachine{
+    #waterAmount = 0;
+    get waterAmount() {
+        return this.#waterAmount;
     }
 
-    get power() {
-        return this._power;
+    set waterAmount(value) {
+        if (value < 0) throw new Error('subzero value');
+        this.#waterAmount = value;
     }
 }
 
-let coffeeMachine = new CoffeeMachine(100);
-console.log(`Power is ${coffeeMachine.power}W`);
+let machine = new CoffeeMachine();
+
+machine.waterAmount = 100;
+console.log(machine.#waterAmount);
